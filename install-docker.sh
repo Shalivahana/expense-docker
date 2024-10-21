d8a2eb37a39f28f713e23ecd7ff66d7ed042ec0a
Chary.manu <chary.manu78@gmail.com>
2024-10-21 21:14:49 +0530

    docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-#check whether root user or not
-R="\e[31m"
-N="\e[0m"
-
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
@@ -11,3 +6,7 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 echo -e "$R Logout and Login again $N"
+
+#check whether root user or not
+R="\e[31m"
+N="\e[0m"
